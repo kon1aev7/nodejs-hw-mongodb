b5f656fb543b8a4b91381161972c84721235d7c4
kon1aev7 <konyaevd1995@gmail.com>
2025-04-07 16:47:32 +0300

    sas

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,3 +1,18 @@
-import ContactCollection from '../db/models/contact.js';
-export const getContacts = () => ContactCollection.find();
-export const getContactsById = (id) => ContactCollection.findOne({ _id: id });
+import ContactCollection from '../db/Models/contact.js';
+
+export const getAllContact = () => ContactCollection.find();
+
+export const getContactById = (contactId) =>
+  ContactCollection.findOne({ _id: contactId });
+
+export const addContact = (payload) => ContactCollection.create(payload);
+
+export const updateContact = async (_id, payload) => {
+  const data = await ContactCollection.findOneAndUpdate({ _id }, payload, {
+    new: true,
+  });
+  return data;
+};
+
+export const deleteContactById = async (contactId) =>
+  await ContactCollection.findOneAndDelete({ _id: contactId });
